9600330b10455bb561c008c05d823f688ae2626d
ulises <ulipaeh@gmail.com>
2020-01-10 11:44:26 -0600

    add examples series

---
+++ Examples series/Binomial Serie.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 28 12:09:11 2019
+
+@author: ulipa
+"""
+import matplotlib.pyplot as plt
+import pandas as pd 
+
+a = 0.9
+N = 50000
+
+xk = []
+
+n_max = 16
+n = [] 
+for i in range(0,N):
+    A   = str.split(bin(i),'0b')[1]
+    aux = A.count('1')
+    n.append(aux)
+     
+for k in range(1,N):
+    xk.append(pow( a, n[k-1])*pow(1-a, n_max-n[k-1]) )
+plt.plot(xk)
+
+df = pd.DataFrame(xk)
+df.to_csv('C:/Users/user/Binomial serie.txt', header = None, index= False, sep = '\t')
+
+
---
+++ Examples series/Random Serie.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 28 13:37:32 2019
+
+@author: ulipa
+"""
+
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt
+import random
+
+x = np.arange(0,50000)
+y = []
+
+for i in x:
+    y.append(random.randint(0,1000))
+    
+plt.plot(y)
+
+df = pd.DataFrame(y)
+df.to_csv('C:/Users/user/Random Serie.txt', header = None, index= False, sep = '\t')
