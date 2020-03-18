e5933cc03b16b320566ff8676fca650446dbf36c
Ulises <ulipaeh@gmail.com>
2020-03-18 09:36:39 -0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 18 09:27:59 2020
+
+@author: ulipa
+"""
+from distutils.core import setup
+setup(
+  name = 'mdfaPy',
+  packages = ['matplotlib','numpy','pandas','pyqt5','sklearn','numba'],
+  version = '1.5',
+  description = 'A multifractal analysis interface',
+  author = 'ulipaeh',
+  author_email = '',
+  url = 'https://github.com/Ulipaeh/mdfaPy',
+  keywords = ['DFA', 'MFDFA'],
+  classifiers = [],
+)
